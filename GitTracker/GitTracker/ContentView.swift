6b7ec8d28a120196fdd62a16c2c0032d356552ad
Zach Chase <zzachchase@gmail.com>
2026-09-15 19:39:21 +0100

    Initial GitTracker project setup

---
+++ GitTracker/GitTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitTracker
+//
+//  Created by Zach Chase on 15/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitTracker/GitTracker/GitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTrackerApp.swift
+//  GitTracker
+//
+//  Created by Zach Chase on 15/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct GitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
