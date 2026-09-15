91fcc9b8f013cb7eeeae8e6d63a2e31a222f65df
Zach Chase <zzachchase@gmail.com>
2026-09-15 20:27:48 +0100

    Added widget stuff.

---
+++ GitTracker/GitTrackerWidget/GitTrackerWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  GitTrackerWidgetBundle.swift
+//  GitTrackerWidget
+//
+//  Created by Zach Chase on 15/09/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct GitTrackerWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        GitTrackerWidget()
+    }
+}
