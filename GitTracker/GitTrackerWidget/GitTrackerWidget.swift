3878fed8d77869035949832362d36dde5c48d9d4
Zach Chase <zzachchase@gmail.com>
2026-09-17 02:13:09 +0100

    Added more hardcoded squares to help me when making the calendar system.

--- GitTracker/GitTrackerWidget/GitTrackerWidget.swift
+++ GitTracker/GitTrackerWidget/GitTrackerWidget.swift
@@ -73,10 +73,14 @@ struct GitTrackerWidgetEntryView : View {
                     ForEach(0..<24, id: \.self) { column in
                         RoundedRectangle(cornerRadius: 2)
                             .fill(
-                                row == 3 && column == 10 ? Color.green.opacity(0.25) :
+                                row == 3 && column == 10 ? Color.green.opacity(0.30) :
                                 row == 3 && column == 11 ? Color.green.opacity(0.50) :
                                 row == 3 && column == 12 ? Color.green.opacity(0.75) :
                                 row == 3 && column == 13 ? Color.green :
+                                row == 1 && column == 16 ? Color.green.opacity(0.50) :
+                                row == 5 && column == 18 ? Color.green :
+                                row == 2 && column == 20 ? Color.green.opacity(0.75) :
+                                row == 6 && column == 21 ? Color.green.opacity(0.30) :
                                 Color.gray.opacity(0.20)
                             )
                             .frame(width: 10, height: 10)
